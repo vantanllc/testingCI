24236b26e25e5e14ae97f58a785ddfe8b6c10865
Vantan LLC <vantan.llc@gmail.com>
2018-03-17 20:04:01 -0500

    run fastlane

--- travis/script.sh
+++ travis/script.sh
@@ -3,8 +3,5 @@ if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
   fastlane ios upload_metadata
   fastlane ios beta
 else
-  open -b com.apple.iphonesimulator
-  fastlane ios test || exit 10
-  fastlane ios report_test_coverage
-  fastlane ios build
+  fastlane match appstore --verbose
 fi
